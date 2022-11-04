bfe7be2aa65dec579cb6b5f3737b1373851db62d
dee-mit-aks <dee.mit.aks@gmail.com>
2022-11-04 13:21:58 +0530

    datafech moved to app.js

--- app.js
+++ app.js
@@ -3,11 +3,74 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var schema = require('./config/connection')
+var db = require('./config/connection')
+var requests =  require('requests')
 
 var db = require('./config/connection')
 db.connect((err)=>{
   if(err) console.log(err);
   else console.log('connected to cloud');
+            var oldIssue = null;
+            var balance = 2000;
+            function dataFetch()
+              {
+                var data;
+                requests('https://earnpredict.com/earnservice.php')
+                .on('data',async function (chunk) {
+                data =await JSON.parse(chunk)
+                data = await data.data
+
+                var newIssue = await data[1].issue
+                var result = await data[1].result
+                var fail = await data[1].fail
+                var amount = await data[1].amount
+                var currAmount =await parseFloat(data[0].amount)
+                var currIssue = await data[0].issue
+                amount = parseFloat(amount)
+              
+
+                console.log(data[0].issue);
+                console.log(data[0].amount);
+                console.log(balance);
+
+
+              if(balance < 1)
+              {
+                  return;
+              }
+
+              if(newIssue != oldIssue)
+              {
+                  balance = balance - currAmount
+                  if(result != 'fail')
+                  {
+                      balance = balance + amount*1.9
+                  }
+                  var final={
+                      'previssue':newIssue,
+                      'prevresult':result,
+                      'fail seq':fail,
+                      'current issue':currIssue,
+                      'amount':currAmount,
+                      'balance':balance
+                  }
+                  console.log(final);
+                  db.predictionDb().collection('result').insertOne(final).then(async()=>{
+                  oldIssue = await newIssue;
+                  dataFetch()
+                  })
+              }
+              else
+              {
+                  dataFetch()
+              }
+              
+
+              })
+              }
+
+      dataFetch()
 })
 
 var indexRouter = require('./routes/index');
--- routes/index.js
+++ routes/index.js
@@ -1,74 +1,13 @@
 var express = require('express');
-var requests =  require('requests')
+
 var router = express.Router();
-var schema = require('../config/connection')
-var db = require('../config/connection')
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-            var oldIssue = null;
-            var balance = 2000;
-            function dataFetch()
-              {
-                var data;
-                requests('https://earnpredict.com/earnservice.php')
-                .on('data',async function (chunk) {
-                data =await JSON.parse(chunk)
-                data = await data.data
-
-                var newIssue = await data[1].issue
-                var result = await data[1].result
-                var fail = await data[1].fail
-                var amount = await data[1].amount
-                var currAmount =await parseFloat(data[0].amount)
-                var currIssue = await data[0].issue
-                amount = parseFloat(amount)
-              
-
-                console.log(data[0].issue);
-                console.log(data[0].amount);
-                console.log(balance);
-
-
-              if(balance < 1)
-              {
-                  return;
-              }
-
-              if(newIssue != oldIssue)
-              {
-                  balance = balance - currAmount
-                  if(result != 'fail')
-                  {
-                      balance = balance + amount*1.9
-                  }
-                  var final={
-                      'previssue':newIssue,
-                      'prevresult':result,
-                      'fail seq':fail,
-                      'current issue':currIssue,
-                      'amount':currAmount,
-                      'balance':balance
-                  }
-                  console.log(final);
-                  db.predictionDb().collection('result').insertOne(final).then(async()=>{
-                  oldIssue = await newIssue;
-                  dataFetch()
-                  })
-              }
-              else
-              {
-                  dataFetch()
-              }
-              
-
-              })
-              }
-
-      dataFetch()
+            
     console.log('hii');
 });
 
